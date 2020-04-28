8d696ac9ede49dd688243781031d51697c1ab66f
Artur Tarcísio <arturtarcisio1@gmail.com>
2020-04-28 19:00:36 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/arturtarcisio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/arturtarcisio/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.arturtarcisio.workshopmongo.resources;
 
+import com.arturtarcisio.workshopmongo.domain.Post;
 import com.arturtarcisio.workshopmongo.domain.User;
 import com.arturtarcisio.workshopmongo.dto.UserDTO;
 import com.arturtarcisio.workshopmongo.services.UserService;
@@ -53,6 +54,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
